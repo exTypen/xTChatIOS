a29b3f33e207f2ccde20cefefb233150723cbb03
extypen <yasirkilinc00@gmail.com>
2023-03-29 13:14:37 +0300

    Initial Commit

---
+++ xTMessage/xTMessageApp.swift
@@ -0,0 +1,17 @@
+//
+//  xTMessageApp.swift
+//  xTMessage
+//
+//  Created by Yasir Kılınç on 29.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct xTMessageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
